8187f674c20050a0c7fc7b9dab36a63a19515c27
Igor Kurchatov <igor83.kurchatov@gmail.com>
2016-12-10 14:01:07 +1100

    diff21 added

---
+++ diff21/diff21_test.py
@@ -0,0 +1,11 @@
+#################################
+# Task 4 - testing
+# Desription: Given an int n, return the absolute difference between n and 21, except return double the absolute difference if n is over 21.
+# Author : Igor Kurchatov 5/12/2016
+#################################
+
+import TestCaseJson
+from diff21 import diff21
+
+testjson = TestCaseJson.TestCaseJson()
+testjson.Start("diff21", r"diff21\diff21_test_case.txt", diff21)
