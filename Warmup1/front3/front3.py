5b834d105f27ae9ac676df81125189843e216d24
Igor Kurchatov <igor83.kurchatov@gmail.com>
2016-12-11 02:11:07 +1100

    front3 added

---
+++ Warmup1/front3/front3.py
@@ -0,0 +1,9 @@
+#################################
+# Task 12 - definitions
+# Desription: Given a string, we'll say that the front is the first 3 chars of the string. 
+# If the string length is less than 3, the front is whatever is there. Return a new string which is 3 copies of the front.
+# Author : Igor Kurchatov 5/12/2016
+#################################
+
+def front3(str):
+  return str[:3] * 3
