b2961bce06008a0513f0b23b7fc00edec7b24692
Igor Kurchatov <igor83.kurchatov@gmail.com>
2016-12-11 01:07:09 +1100

    Delete diff21_run.py

--- diff21/diff21_run.py
+++
@@ -1,9 +0,0 @@
-#################################
-# Task 4 - implementation
-# Desription: Given an int n, return the absolute difference between n and 21, except return double the absolute difference if n is over 21.
-# Author : Igor Kurchatov 5/12/2016
-#################################
-
-from diff21 import diff21_run
-
-diff21_run()
