26106082c7cfff717106eafaf9f52cc774fbdc6d
Igor Kurchatov <igor83.kurchatov@gmail.com>
2016-12-10 13:24:57 +1100

    Update sleep_in_run.py

--- sleep_in/sleep_in_run.py
+++ sleep_in/sleep_in_run.py
@@ -1,6 +1,6 @@
 #################################
 # Task 1 - implementation
-# Desription: We sleep in if it is not a weekday or we're on vacation. Return True if we sleep in. Running the method.
+# Desription: We sleep in if it is not a weekday or we're on vacation. Return True if we sleep in.
 # Author : Igor Kurchatov 5/12/2016
 #################################
 
