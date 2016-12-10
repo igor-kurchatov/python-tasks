295d5441913d676ba6d5621eee481ba0f537d3e6
Igor Kurchatov <igor83.kurchatov@gmail.com>
2016-12-11 00:49:54 +1100

    not_string added

---
+++ not_string/not_string_run.py
@@ -0,0 +1,10 @@
+#################################
+# Task 9 - implementation
+# Desription: Given a string, return a new string where "not " has been added to the front.
+# However, if the string already begins with "not", return the string unchanged.
+# Author : Igor Kurchatov 5/12/2016
+#################################
+
+from not_string import not_string_run
+
+not_string_run()
