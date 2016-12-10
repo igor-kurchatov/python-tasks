c810a3d845a90aac84d9c758c0b877132158af9c
Igor Kurchatov <igor83.kurchatov@gmail.com>
2016-12-10 13:28:52 +1100

    sum_double added

---
+++ sum_double/sum_double.py
@@ -0,0 +1,28 @@
+#################################
+# Task 3 - definitions
+# Desription: Given two int values, return their sum. Unless the two values are the same, then return double their sum.
+# Author : Igor Kurchatov 5/12/2016
+#################################
+
+def sum_double(a, b):
+  if a == b:
+    return (a + b) * 2
+  else:
+    return a + b
+
+def sum_double_run():
+  
+  print("Given two int values, it's returned with the sum. Unless the two values are the same, then the sum will be doubled.")
+    
+  inp_first_num = 0.0
+  inp_second_num = 0.0
+
+  try:
+    inp_first_num = float(input("Please, enter the first number: "))
+    inp_second_num = float(input("Please, enter the second number: "))
+  except ValueError as e:
+      print(str(e))
+      sum_double_run()
+      return
+
+  print(str.format("The result is {0}", sum_double(inp_first_num, inp_second_num)))
---
+++ sum_double/sum_double_run.py
@@ -0,0 +1,9 @@
+#################################
+# Task 3 - implementation
+# Desription: Given two int values, return their sum. Unless the two values are the same, then return double their sum.
+# Author : Igor Kurchatov 5/12/2016
+#################################
+
+from sum_double import sum_double_run
+
+sum_double_run()
