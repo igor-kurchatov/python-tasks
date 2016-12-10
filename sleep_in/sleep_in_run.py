a37e64efccade4cec752d90d46eea236c880221a
Igor Kurchatov <igor83.kurchatov@gmail.com>
2016-12-11 01:11:31 +1100

    Delete sleep_in_run.py

--- sleep_in/sleep_in_run.py
+++
@@ -1,9 +0,0 @@
-#################################
-# Task 1 - implementation
-# Desription: We sleep in if it is not a weekday or we're on vacation. Return True if we sleep in.
-# Author : Igor Kurchatov 5/12/2016
-#################################
-
-import sleep_in
-
-sleep_in.sleep_in_run()
