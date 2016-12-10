2ae03e0b03a79a87cd97eac3ccfe9a6fd45367c1
Igor Kurchatov <igor83.kurchatov@gmail.com>
2016-12-10 13:23:14 +1100

    Update monkey_trouble_run.py

--- monkey_trouble/monkey_trouble_run.py
+++ monkey_trouble/monkey_trouble_run.py
@@ -1,8 +1,7 @@
 #################################
-# Task 2
+# Task 2 - implementation
 # Desription: We have two monkeys, a and b, and the parameters a_smile and b_smile indicate if each is smiling. 
 # We are in trouble if they are both smiling or if neither of them is smiling. Return True if we are in trouble.
-# Running the method. 
 # Author : Igor Kurchatov 5/12/2016
 #################################
 
