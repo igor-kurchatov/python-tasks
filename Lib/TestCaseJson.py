489c98d7df07a004d20cf571c3d8dc63e1243bbe
Igor Kurchatov <igor83.kurchatov@gmail.com>
2016-12-28 00:41:57 +1100

    Update TestCaseJson.py
    Added ability to perform multiple test cases read from a specified module.

--- Lib/TestCaseJson.py
+++ Lib/TestCaseJson.py
@@ -1,28 +1,46 @@
 #################################
 # unittest class modified to load settings from json file 
-# Desription: If you need to test your method it needs json file to perform test with predefined parameteres(arguments) of the test function 
+# Desription: If you need to test your methods it needs json file to perform test within predefined parameteres(arguments) of the test functions 
 
-# Test case file format: 
-#{"paramNames": [<argumentName1>,...,<argumentNameN>],                                        #list of argument names 
-# "testCases": [{<argument1>: <value1>,...,<argumentN>: <valueN>, "result": <resultValue>},   #list of test scenarios
+# Single test case file format: 
+# {"testCases": [{<argument1>: <value1>,...,<argumentN>: <valueN>, "result": <resultValue>},   #list of test scenarios
 #               ...
 #               {<scenarioN>}
 #		      ]
-#}
+# }
 
-#Sample json test file
-#{"paramNames": ["weekday", "vacation"], 
-# "testCases": [{"weekday": false, "vacation": false, "result": true},
+# Single sample json test file
+# {"testCases": [{"weekday": false, "vacation": false, "result": true},
 #               {"weekday": true, "vacation": false, "result": false}
 #		      ]
-#}
+# }
 
+# Multiple test case file format: 
+# {"<func_name1>": [{<argument1>: <value1>,...,<argumentN>: <valueN>, "result": <resultValue>},   #list of test scenarios
+#                 ...
+#                  {<scenarioN>}
+#		          ],
+#  "<func_nameN>": [...]
+# }
+
+# Multiple sample json test file
+# {"string_times": [{"str": "Hi", "n": 2, "result": "HiHi"},
+# 		            {"str": "Hi", "n": 3, "result": "HiHiHi"},
+#				    {"str": "Hi", "n": 1, "result": "Hi"}
+#		           ],
+# "front_times": [{"str": "Chocolate", "n": 2, "result": "ChoCho"},
+#		          {"str": "Chocolate", "n": 3, "result": "ChoChoCho"},
+#				  {"str": "Abc", "n": 3, "result": "AbcAbcAbc"}  
+#		         ]
+# }
 
 # Author : Igor Kurchatov 8/12/2016
 #################################
 
 import unittest
 import json
+import inspect
+from collections import OrderedDict
 
 from enum import Enum
 
@@ -43,91 +61,126 @@ class TestCaseJson(unittest.TestCase):
     _defaultTest = "defaultTest"
     _result = "result"
     
-    def __choose_assert_func__(self, assertIndex, func, result, *args):
+    def __choose_assert_func__(self, assertIndex, func, result, kwargs):
         
         if assertIndex == AssertMethods.assertEqual:
-           self.assertEqual(func(*args), result)
+           self.assertEqual(func(**kwargs), result)
         elif assertIndex == AssertMethods.assertTrue:
-            res = func(*args)
-            self.assertTrue(res)
+            self.assertTrue(func(**kwargs))
         else:
             pass                        
     
-    def __loadjson__(self):
+    def __loadjson__(self, fileName):
         
-        if self.fileName == "":
+        if fileName == "":
             return
 
         #loading json from file
         try:
-            f_data = open(self.fileName).read()
+            f_data = open(fileName).read()
         except FileNotFoundError as e:
             print("Test case file is not found. " + str(e))
             return
 
-        #getting json object from string
-        self.jsonData = json.loads(f_data);
-        
-        #storing in self
-        self.paramNames = self.jsonData[self._paramNames]
-        self.testCases = self.jsonData[self._testCases]
+        self.jsonData = json.loads(f_data)
     
     #test initialization
     def __init__(self, *args, **kwargs):     
         super(TestCaseJson, self).__init__(*args, **kwargs)
 
         self.userFunc = None
-        self.userFuncName = ""
         self.testName = ""
-        self.fileName = ""
         self.assertMethod = None
         self.jsonData = None
-        self.paramNames = None
-        self.testCases = None
+        self.module = None
+        self.testCases = {}
+
+    #actions before the test
+    def setUp(self):
+        return super().setUp()
+
+    #actions after the test
+    def tearDown(self):
+        return super().tearDown()
+
+    #getting new dict from json file with func arguments and values
+    def get_func_paramvalues(self, func, params):
+        
+        if func is None:
+            return None
+                       
+        arginfo = {}
+        arg_names = {}
+        values = {}
+        arg_names_len = 0
+        arg_defaults_len = 0
+
+        try:
+            arginfo = inspect.getargspec(func)
+            if arginfo.args is not None:
+                arg_names = arginfo.args
+                arg_names_len = len(arg_names)
+            if arginfo.defaults is not None:
+                arg_defaults_len = len(arginfo.defaults)
+        except TypeError:
+            print("Error in reading arguments from function.")
+            return None                
+                    
+        values = OrderedDict.fromkeys(arg_names)
+
+        for arg in arg_names:                    
+            values[arg] = dict(params).get(arg)             
+
+        if arg_names_len - arg_defaults_len > len(values):
+            print("Not enough parameters to send to the function {}.".format(func.__name__))
+            return None
+        
+        return values
+
+    #return a pair of key = value
+    def getFormattedDictString(self, pv):
+        l = list()
+        for k,v in pv.items():
+            l.append('{} = {}'.format(k,str(v)))
+        return ','.join(map(str,l))
     
-    #test start
+    #single test start
     def __runTest__(self):
-        args = []               #arguments
+        
         res = None              #result
-        strParams = ""          #arguments string 
+        pvalues = {}            #arguments with values
+        strParams = ""          #string of arguments separated by comma   
         currTestCaseNum = 0     #current test
         failedCount = 0         #count of failed test cases
+
+        self.setUp()            #some actions before the test
         
-        if self.testCases is None:
-            print("Test cases is not found.")
-            return
-
         #test case count info
-        print("\nTest case {} starts. Number of test cases: {}".format(self.testName, len(self.testCases)))
-
-        if self.userFunc is None:
-            print("User function is not defined. Test can't be completed.")
-            return
+        print("\nTest case \"{}\" starts. Number of test cases: {}".format(self.testName, len(self.testCases)))        
         
         #test cases loop
-        for v in self.testCases:
+        for tc_params in self.testCases:
            
             currTestCaseNum += 1
             
-            #clear the list of arguments
-            args.clear()
-           
-            #arguments
-            for p in self.paramNames:
-                args.append(v[p])                  
+            pvalues = self.get_func_paramvalues(self.userFunc, tc_params)
+            
+            if not pvalues:
+                return
             
             #the result
-            res = v[self._result]
+            res = tc_params[self._result]            
             
-            #convert arguments to string using mapping       
-            strParams = ','.join(map(str, args))            
+            #convert arguments to string using mapping
+            strParams = self.getFormattedDictString(pvalues)           
+                              
             try:                
                 
                 #test case brief info
-                print("{}. Performing {}({}) => {}".format(currTestCaseNum, self.userFuncName, strParams, res))
+                print("{}. Start {}({}) => {}".format(currTestCaseNum, self.userFunc.__name__, strParams, res))
                 
                 #running the test
-                self.__choose_assert_func__(self.assertMethod, self.userFunc, res, *args)
+                self.__choose_assert_func__(self.assertMethod, self.userFunc, res, pvalues)
                                             
             except AssertionError as e:
                 #in case of exception just print the formatted text
@@ -138,35 +191,97 @@ class TestCaseJson(unittest.TestCase):
                 print("Status: " + self._succeed)
         
         #test case finishes
-        if failedCount == 0:
-            print("Test case completed successfully.")
-        else:
-            print("Test case failed. {} failure(s) occured.".format(failedCount))     
-    
-    def setUp(self):
-        return super().setUp()
-    
-    def tearDown(self):
-        return super().tearDown()                
+        print("Result: {} / {} completed successfully.".format(len(self.testCases) - failedCount, len(self.testCases)))
+        
+        self.tearDown()            #some actions after the test     
+                            
+    #start multiple test cases from a module
+    def __runAllTest__(self):
+        
+        res = None              #result
+        pvalues = {}            #arguments with values
+        strParams = ""          #string of arguments separated by comma 
+        testCases = {}          #test cases    
+        currTestCaseNum = 0     #current test
+        failedCount = 0         #count of failed test cases
+        func_list = list()      #list of all test functions from a module
+                
+        for name in dir(self.module):
+            obj = getattr(self.module, name)
+            if inspect.ismethod(obj) or inspect.isfunction(obj):
+              func_list.append(obj)
+        
+        if len(func_list) == 0:
+            print("There are no test functions were found.")
+            return
+        
+        #start each test function
+        for f in func_list:
+            
+            self.testCases = dict(self.jsonData).get(f.__name__)  
+            
+            #self.testCases = self.jsonData[f.__name__]
 
+            if not self.testCases:
+                continue
+            
+            self.userFunc = f
+            self.testName = f.__name__
+
+            self.__runTest__() #run a single function test
+                
+    #entry to start test case
     def Start(self, testName = _defaultTest, fileName = "", userFunc = None, assertMethod = AssertMethods.assertEqual):
         
         #settings
         self.userFunc = userFunc
-        if self.userFunc is not None:             
-            self.userFuncName = self.userFunc.__name__
         self.testName = testName
-        self.fileName = fileName
         self.assertMethod = assertMethod
         self.jsonData = None
-        self.paramNames = None
-        self.testCases = None
 
         #read arguments from json
-        self.__loadjson__()
+        self.__loadjson__(fileName)
+
+        if self.jsonData is None:
+            print("Test file is empty.")
+            return
+
+        #reading test case scenarios
+        self.testCases = self.jsonData[self._testCases]
+
+        if self.testCases is None:
+            print("There are not any test cases to perform.")
+            return
+
+        if self.userFunc is None:
+            print("User function is not defined.")
+            return
 
         #test case implementation
         case = unittest.FunctionTestCase(self.__runTest__, self.setUp, self.tearDown)
                 
         #suite = unittest.TestLoader().loadTestsFromName(TestCaseJson.__runTest__, TestCaseJson)
+        caseResult = unittest.TextTestRunner(verbosity=2).run(case)
+
+    #entry to start all test cases from chosen module
+    def StartAll(self, module = None, fileName = "", assertMethod = AssertMethods.assertEqual):
+        
+        if module is None:
+            print("Test module is not found.")
+            return
+        
+        self.module = module
+        self.assertMethod = assertMethod
+        self.jsonData = None        
+
+        #read test parameteres from json file
+        self.__loadjson__(fileName)
+
+        if self.jsonData is None:
+            print("Test file is empty.")
+            return
+      
+        #test case implementation
+        case = unittest.FunctionTestCase(self.__runAllTest__, self.setUp, self.tearDown)
+                
         caseResult = unittest.TextTestRunner(verbosity=2).run(case)
